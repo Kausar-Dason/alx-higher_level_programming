9aaabae655de4c19ebba0fb7fd5f09e365f1db4d
Kausar-Dason <iddidasonkausar2002@gmail.com>
2024-12-20 10:51:09 -0800

    Write a script that prints 3 lines:

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let r = "C is fun"
-let s = "Python is cool"
-let t = "JavaScript is amazing"
+let r = "C is fun";
+let s = "Python is cool";
+let t = "JavaScript is amazing";
 console.log(r);
 console.log(s);
 console.log(t);
